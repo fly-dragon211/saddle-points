596f30d855be88bf5f402bb5d84a386cc153dc33
Michael Hutcheon <mjh261@pc62.tcm.phy.cam.ac.uk>
2019-01-25 13:47:12 +0000

    Algorithm is now scale-independent

--- castep/refactor/saddle-point-castep.py
+++ castep/refactor/saddle-point-castep.py
@@ -155,9 +155,10 @@ class parameter:
 class cell:
 	def __init__(self, cellfile):
 
-		# Parse a cell from a cellfile
 		self.params = []
+		self.pot_evals = 0
 
+		# Parse a cell from a cellfile
 		# Parse line-by-line, blanking out lines that are parsed
 		lines = [l.lower() for l in open(cellfile).read().split("\n")]
 		for i, line in enumerate(lines):
@@ -289,12 +290,13 @@ class cell:
 		for x in xs:
 			for y in ys:
 				pot += 1/la.norm(np.array([x,y])-disps)
-		return pot
+		return pot * 100
 
 	def potential(self):
-		return self.test_pot_coulomb(rotation=2)
+		self.pot_evals += 1
+		return self.test_pot_coulomb(rotation=0)
 
-	def line_minimize(self, direction, init_pot=None):
+	def line_minimize(self, direction, init_pot=None, max_disp=np.inf, normalize=True):
 		
 		# Minimize the cell potential along the
 		# direction given in variable parameter
@@ -307,7 +309,7 @@ class cell:
 
 		if init_pot == None: init_pot = self.potential()
 
-		direction /= la.norm(direction)
+		if normalize: direction /= la.norm(direction)
 		init_pars = [p.value for p in vp]
 
 		disps = [0]
@@ -319,15 +321,19 @@ class cell:
 			if n not in [0, len(pots)-1]: break
 
 			if n == 0:
+				if abs(disps[0]-0.05) > max_disp: break
 				disps.insert(0, disps[0]-0.05)
 				for i, v in enumerate(init_pars):
 					vp[i].value = v + disps[0]*direction[i]
 				pots.insert(0, self.potential())
 			else:
+				if abs(disps[-1]+0.05) > max_disp: break
 				disps.append(disps[-1]+0.05)
 				for i, v in enumerate(init_pars):
 					vp[i].value = v + disps[-1]*direction[i]
 				pots.append(self.potential())
+
+		if len(disps) < 2: return
 
 		int_x = np.linspace(min(disps)+0.001, max(disps)-0.001, 100)
 		int_y = interp_new(int_x, disps, pots)
@@ -342,16 +348,35 @@ class cell:
 		plt.axvline(int_x[mini])
 		plt.show()
 
+	def line_min_disp(self, direction, max_disp=np.inf):
+		direction /= la.norm(direction)
+		for i, par in enumerate(self.variable_params()):
+			direction[i] *= par.scale
+		self.line_minimize(direction, normalize=False, max_disp=max_disp)
+
+	def config(self):
+		return np.array([p.value/p.scale for p in self.variable_params()])
+
+	def init_config(self):
+		return np.array([p.init_val/p.scale for p in self.variable_params()])
+
+	def set_config(self, cfg):
+		for i, par in enumerate(self.variable_params()):
+			par.value = cfg[i]*par.scale
+
 pot_already_plotted = False
 def plot_potential(cell):
 	global pot_already_plotted
 	if pot_already_plotted: return
 	pot_already_plotted = True
-	X_RANGE = cell.variable_params()[0].scale*2
-	Y_RANGE = cell.variable_params()[1].scale*2
+	vp = cell.variable_params()
+	X_RANGE  = vp[0].scale*2
+	Y_RANGE  = vp[1].scale*2
+	xc = vp[0].init_val
+	yc = vp[1].init_val
         RESOL = 100
-        x   = np.linspace(-X_RANGE,X_RANGE,RESOL)
-        y   = np.linspace(-Y_RANGE,Y_RANGE,RESOL)
+        x   = np.linspace(-X_RANGE+xc,X_RANGE+xc,RESOL)
+        y   = np.linspace(-Y_RANGE+yc,Y_RANGE+yc,RESOL)
         z   = []
         all_vals = []
         max_val = -np.inf
@@ -360,8 +385,8 @@ def plot_potential(cell):
         for yi in y:
                 row = []
                 for xi in x:
-			vp[0].value = vp[0].init_val + xi
-			vp[1].value = vp[1].init_val + yi
+			vp[0].value = xi
+			vp[1].value = yi
 			val = cell.potential()
                         row.append(val)
                         all_vals.append(val)
@@ -373,135 +398,116 @@ def plot_potential(cell):
         x,y = np.meshgrid(x,y)
         levels = np.linspace(0,1,30)
         plt.contour(x,y,z,cmap="tab20c",levels=levels)
-        plt.imshow(z,cmap="tab20c",extent=(-X_RANGE,X_RANGE,-Y_RANGE,Y_RANGE),
-                   origin="lower",interpolation="bilinear",alpha=0.2)
-        plt.xlabel("x")
-        plt.ylabel("y")
+        plt.imshow(z,cmap="tab20c",extent=(-X_RANGE+xc,X_RANGE+xc,-Y_RANGE+yc,Y_RANGE+yc),
+                   origin="lower",interpolation="bilinear",alpha=0.2,aspect="auto")
+        plt.xlabel(vp[0].name)
+        plt.ylabel(vp[1].name)
 
 
 class path_info:
 	
 	def __init__(self):
-		self.pot       = None
-		self.disp      = None
-		self.force     = None
-		self.norm      = None
-		self.fpara     = None
-		self.fperp     = None
-		self.dx        = None
+		self.pot        = None
+		self.norm       = None
+		self.force      = None
+		self.fpara      = None
+		self.fperp      = None
+		self.config     = None
+		self.line_min   = None
+		self.pot_evals  = None
+		self.activation = None
 
 def find_saddle_point(cell):
 
-	title = "Begin saddle point search"
-	div = "".join(["%" for c in title]) 
-	print div + "\n" + title + "\n" + div
-	max_par_name_len = max([len(p.name) for p in cell.variable_params()])
+	step_size = 0.1
 
-	var_param_count = len(cell.variable_params())
-	max_ss  = 0.1
 	path = []
 
-	# Perturb the cell by an initial random amount
-	for i, p in enumerate(cell.variable_params()):
-		cell.perturb(p, max_ss*(np.random.rand()*2-1))
+	init_cfg = cell.config()
+	init_cfg += step_size*(np.random.rand(len(init_cfg))*2-1)
+	cell.set_config(init_cfg)
 
-	for step_index in range(0,25):
+	for step_index in range(0,100):
+
+		p = path_info()
 		
-		print "\nIteration "+str(step_index+1)
+		p.config    = cell.config()
+		p.pot       = cell.potential()
+		p.force     = np.zeros(len(p.config))
+		p.pot_evals = cell.pot_evals
 
-		p       = path_info()
-		p.pot   = cell.potential()
-		p.force = np.zeros(var_param_count)
-		p.disp  = np.zeros(var_param_count)
+		for i in range(0, len(p.config)):
+			ei    = np.zeros(len(p.config))
+			eps   = step_size / 10
+			ei[i] = eps
+			cell.set_config(p.config + ei)
+			p.force[i] = -(cell.potential() - p.pot)/eps
 
-		print "    Potential  : "+str(p.pot)+" eV"
-		tf = "    {0:{1}}   {2:15}   {3:16}"
-		header = tf.format("Parameter", max_par_name_len, "Value", "Force")
-		tdiv = "    "+"".join(["~" for char in header])[0:-4]
-		print "\n    Force evaluation..."
-		print tdiv + "\n" + header + "\n" + tdiv
-
-		for i, par in enumerate(cell.variable_params()):
-			
-			# Caclulate the force on the parameter
-			# using finite differences
-			eps = max_ss * par.scale / 10.0
-			cell.perturb(par, eps)
-			grad = cell.potential() - p.pot
-			cell.perturb(par, -eps)
-			p.force[i] = -grad
-
-			tf = "    {0:{1}} | {2:10.9e} | {3:10.9e}"
-			print tf.format(par.name, max_par_name_len, par.value, p.force[i])
-
-			# Record the displacement of this
-			# parameter from it's initial value
-			p.disp[i] = par.value - par.init_val
-
-		print tdiv
-		print "    |Force| = "+str(la.norm(p.force))
-
-		p.norm = p.disp / la.norm(p.disp)
-
-		# Calculate the components of the force which
-		# are parrallel and perpendicular to p.norm
-		p.fpara = np.dot(p.force, p.norm) * p.norm
+		p.norm  = p.config - cell.init_config()
+		p.norm /= la.norm(p.norm)
+		p.fpara = np.dot(p.force, p.norm)*p.norm
 		p.fperp = p.force - p.fpara
 
-		# Create a displacement along
-		# the perpendicular force, but against the
-		# parallel one
-		p.dx = p.fperp - p.fpara
-		p.dx = max_ss * p.dx / la.norm(p.dx)
+		p.activation = p.fperp - p.fpara
+		p.activation = step_size * p.activation / la.norm(p.activation)
 
-		# Take the step
-		print "\n    Step evaluation...\n"+tdiv
-		print "    {0:{1}}   {2:12}".format("Parameter",max_par_name_len,"Step")
-		print tdiv
-		for i, par in enumerate(cell.variable_params()):
-			cell.perturb(par, p.dx[i])
-			print "    {0:{1}} | {2:10.9e}".format(par.name, max_par_name_len, p.dx[i])
-		print tdiv
+		cell.set_config(p.config + p.activation)
+		cell.line_min_disp(p.fperp, 0.2)
+		p.line_min = cell.config() - p.config - p.activation
 
-		# Carry out a line minimization along the p.fperp direction
-		cell.line_minimize(p.fperp)
-		
-		# Record information about this step
+		if len(path) > 0:
+			if np.dot(p.activation, path[-1].activation) < 0:
+				step_size /= 2
+				p.activation /= 2
+			if la.norm(p.force) < la.norm(path[0].force):
+				path.append(p)
+				break
+
 		path.append(p)
 
-	if var_param_count == 2:
+	return path
 
-		plt.subplot(1,2,1)
-		plot_potential(cell)
-		xs = [p.disp[0] for p in path]
-		ys = [p.disp[1] for p in path]
-		plt.plot(xs,ys,marker="+")
-		for p in path: 
-			perp_norm = p.fperp/la.norm(p.fperp)
-			plt.plot(*zip(*[p.disp, p.disp+perp_norm/10]), color="green")
-			plt.plot(*zip(*[p.disp, p.disp+p.norm/10]), color="black")
-		
-		plt.subplot(4,4,3)
-		plt.plot([p.pot for p in path])
-		plt.xlabel("Iteration")
-		plt.ylabel("Potential")
+def plot_path_info(path, cell):
 
-		plt.subplot(4,4,4)
-		plt.plot([la.norm(p.force) for p in path])
-		plt.xlabel("Iteration")
-		plt.ylabel("|Force|")
+	print "Length of path: ",len(path)
+	plt.subplot(1,2,1)
+	plot_potential(cell)
+	scales = [par.scale for par in cell.variable_params()]
 
-		plt.subplot(4,4,7)
-		plt.plot([la.norm(path[i].disp-path[i-1].disp) for i in range(1,len(path))])
-		plt.xlabel("Iteration")
-		plt.ylabel("Step size")
+	for p in path: 
+		cf = p.config * scales
+		ac = p.activation   * scales
+		lm = p.line_min * scales
+		plt.plot(*zip(*[cf, cf + ac]), color="red")
+		plt.plot(*zip(*[cf + ac, cf + ac + lm]), color="blue")
+	
+	plt.subplot(4,4,3)
+	plt.plot([p.pot for p in path])
+	plt.xlabel("Iteration")
+	plt.ylabel("Potential")
+
+	plt.subplot(4,4,4)
+	plt.plot([la.norm(p.force) for p in path])
+	plt.xlabel("Iteration")
+	plt.ylabel("|Force|")
+
+	plt.subplot(4,4,7)
+	plt.plot([la.norm(path[i].config-path[i-1].config) for i in range(1,len(path))])
+	plt.xlabel("Iteration")
+	plt.ylabel("Step size\n(Normalized)")
+
+	plt.subplot(4,4,8)
+	plt.plot([p.pot_evals for p in path])
+	plt.xlabel("Iteration")
+	plt.ylabel("Potential evaluations\n(Cumulative)")
 
 # Run program
 cell = cell(sys.argv[1])
-cell.fix_lattice_params("C")
-cell.fix_lattice_angles("ABC")
+cell.fix_lattice_params("AB")
+cell.fix_lattice_angles("AB")
 cell.fix_atoms()
 print parameter.table(cell.params, title = "Initial cell parameters")
 print "\n"+cell.formatted_cell_file(title = "Example generated cell file")+"\n"
-find_saddle_point(cell)
+path = find_saddle_point(cell)
+plot_path_info(path, cell)
 plt.show()
