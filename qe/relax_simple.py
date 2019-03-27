19c85a5073a3489ba10e9446651f2afdbc8152e0
Michael Hutcheon <michael.hutcheon@hotmail.co.uk>
2019-03-27 16:20:27 +0000

    Added a tolerance to relax_simple.py

--- qe/relax_simple.py
+++ qe/relax_simple.py
@@ -1,7 +1,18 @@
 from cell import cell
+import numpy as np
 import sys
+import os
 
+os.system("rm -r singlepoints")
 c = cell(sys.argv[1])
+
+if False:
+	# Perturb the first atom a little (for testing)
+	disp = []
+	for i in range(0, len(c.atoms)):
+		disp.append(np.zeros(3))
+	disp[0] = [0.1,0,0]
+	c.perturb_atoms_cart(disp)
 
 for i in range(0,100):
 
@@ -15,3 +26,7 @@ for i in range(0,100):
 	# Simply follow the force downhill
 	c.perturb_atoms_cart(pert)
 	c.apply_strain_cart(c.stress_cart/1000)
+
+	if c.total_force  > 10e-8: continue
+	if c.total_stress > 0.01: continue
+	break
