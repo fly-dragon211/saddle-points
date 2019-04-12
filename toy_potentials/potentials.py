c47885447401283a5d49aa509c011735e8f21e99
miicck <mjh261@cam.ac.uk>
2019-04-12 16:22:38 +0100

    Played with kriging

--- toy_potentials/potentials.py
+++ toy_potentials/potentials.py
@@ -6,17 +6,20 @@ def eggbox(x):
 	x1 = x[1]*np.pi
 	return np.sin(x0)*np.cos(x1)# + 0.1*np.sin(x0+x1)
 
+pot_evals = 0
 def pot(x):
+	global pot_evals
+	pot_evals += 1
 	return eggbox(x)
 
-def grad_fd(x):
+def grad_fd(x, f=pot):
 	EPS = 0.01
-	p0 = pot(x)
-	px = pot([x[0]+EPS, x[1]])
-	py = pot([x[0], x[1]+EPS])
+	p0 = f(x)
+	px = f([x[0]+EPS, x[1]])
+	py = f([x[0], x[1]+EPS])
 	return np.array([px-p0,py-p0])/EPS
 
-def hess_fd(x):
+def hess_fd(x, f=pot):
 	EPS = 0.01
 	hess = np.identity(len(x), float)
 	for i in range(0,len(x)):
@@ -25,7 +28,7 @@ def hess_fd(x):
 		for j in range(0,len(x)):
 			dj = np.zeros(len(x))
 			dj[j] = EPS
-			hess[i][j] = pot(x+di+dj)-pot(x+di-dj)-pot(x+dj-di)+pot(x-di-dj)
+			hess[i][j] = f(x+di+dj)-f(x+di-dj)-f(x+dj-di)+f(x-di-dj)
 	hess = 0.5 * (hess + hess.T)
 	return hess / (4*EPS**2)
 
@@ -35,7 +38,7 @@ def grad(x):
 def hess(x):
 	return hess_fd(x)
 
-def plot():
+def plot(to_plot=pot):
 	x = np.linspace(-1.5,1.5,100)
 	y = np.linspace(-1.5,1.5,100)
 	x,y = np.meshgrid(x,y)
@@ -43,7 +46,7 @@ def plot():
 	for i in range(0,len(x)):
 		row = []
 		for j in range(0,len(x[i])):
-			val = pot([x[i][j],y[i][j]])
+			val = to_plot([x[i][j],y[i][j]])
 			row.append(val)
 		z.append(row)
 	cs = plt.contour(x,y,z)
